11ab9ad484ee84765bf3a6dc6bb8d0eea6154d43
kanrun-digital <velfit@gmail.com>
2026-03-16 20:47:46 +0200

    Update sidebar.tsx

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,7 +1,13 @@
 "use client"
 
+import React from "react";
+
 export function SidebarProvider({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return <div className="flex min-h-screen">{children}</div>;
+}
+
+export function SidebarInset({ children }: { children: React.ReactNode }) {
+  return <div className="flex-1 flex flex-col min-w-0">{children}</div>;
 }
 
 export function SidebarTrigger({ className, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
