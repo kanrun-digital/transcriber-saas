9a85cb67707fa5bae8c8ab64d373eca0de5b0811
kanrun-digital <velfit@gmail.com>
2026-03-19 23:57:24 +0200

    Update page.tsx

--- src/app/(app)/transcriptions/[id]/page.tsx
+++ src/app/(app)/transcriptions/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState, useCallback } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { transcriptionsService } from "@/services/transcriptions";
